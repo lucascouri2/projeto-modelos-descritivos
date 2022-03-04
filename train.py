96dce74b30e22123b1f25c7e181a2c9f37cc3fcd
Mariama Oliveira <mariama.oliveira@yahoo.com.br>
2022-03-04 10:59:45 -0300

    Adicionando input tamanho

--- train.py
+++ train.py
@@ -16,8 +16,13 @@ def load_fsdd(spectrograms_path):
 
 
 def train(x_train, learning_rate, batch_size, epochs):
+    shape = x_train.shape
+    dimention_1 = shape[1]
+    dimention_2 = shape[2]
+
+
     autoencoder = VAE(
-        input_shape=(256, 64, 1),
+        input_shape=(dimention_1, dimention_2, 1),
         conv_filters=(512, 256, 128, 64, 32),
         conv_kernels=(3, 3, 3, 3, 3),
         conv_strides=(2, 2, 2, 2, (2, 1)),
